0608f3fe48362310eaef76a414b42d7d3bf5e38b
QazbekR <qzrysbekov@gmail.com>
2023-05-08 14:17:39 +0600

    написал прогу и коммит

--- finalTask/Program.cs
+++ finalTask/Program.cs
@@ -1,1 +1,27 @@
 ﻿
+string[] array1 = new string[7] {"1996", "1", "cawabanga", "4536", "why", "because", "nopeYEs"};
+string[] array2 = new string[array1.Length];
+
+
+void arrayT(string[] array1, string[] array2)
+    {
+        int count = 0;
+        for (int i = 0; i < array1.Length; i++)
+            {
+                if(array1[i].Length <= 3)
+                    {
+                        array2[count] = array1[i];
+                        count++;
+                    }
+            }
+    }
+void PrintAr(string[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+    }
+arrayT(array1, array2);
+PrintAr(array2);
